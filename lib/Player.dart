bd84669bd5956261560566b7b917232266980871
Kevin <herrbach.kevin@gmail.com>
2024-02-07 18:30:21 +0900

    Continues tutorial

--- lib/Player.dart
+++ lib/Player.dart
@@ -1,15 +1,41 @@
-import 'package:flame/components.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Player extends PositionComponent {
-  static final _paint = Paint()..color = Colors.white;
+import 'package:flame/components.dart';
+import 'package:seoul/Bullet.dart';
+import 'package:seoul/main.dart';
+
+class Player extends SpriteAnimationComponent with HasGameReference<SeoulGame> {
+  late final SpawnComponent _bulletSpawner;
+  Player() : super(size: Vector2(100, 150), anchor: Anchor.center);
 
   @override
-  void render(Canvas canvas) {
-    canvas.drawRect(size.toRect(), _paint);
+  FutureOr<void> onLoad() async {
+    await super.onLoad();
+    animation = await game.loadSpriteAnimation(
+        'player.png',
+        SpriteAnimationData.sequenced(
+            amount: 4, stepTime: .2, textureSize: Vector2(32, 48)));
+    position = game.size / 2;
+
+    _bulletSpawner = SpawnComponent(
+        factory: (index) {
+          return Bullet(position: position);
+        },
+        period: .2,
+        selfPositioning: true,
+        autoStart: false);
+    add(_bulletSpawner);
   }
 
   void move(Vector2 delta) {
     position.add(delta);
+  }
+
+  void startShooting() {
+    _bulletSpawner.timer.start();
+  }
+
+  void stopShooting() {
+    _bulletSpawner.timer.stop();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame/parallax.dart';
 import 'package:flutter/material.dart';
 import 'package:seoul/Player.dart';
 
@@ -12,17 +13,38 @@ class SeoulGame extends FlameGame with PanDetector {
   @override
   FutureOr<void> onLoad() async {
     await super.onLoad();
-    player = Player()
-      ..position = size / 2
-      ..width = 50
-      ..height = 100
-      ..anchor = Anchor.center;
+
+    final parallax = await loadParallaxComponent(
+      [
+        ParallaxImageData('stars.png'),
+        ParallaxImageData('stars.png'),
+        ParallaxImageData('stars.png'),
+      ],
+      baseVelocity: Vector2(0, -5),
+      repeat: ImageRepeat.repeat,
+      velocityMultiplierDelta: Vector2(0, 5),
+    );
+    add(parallax);
+
+    final playerSprite = await loadSprite('player.png');
+    playerSprite.srcSize = Vector2(32.0, 39.0);
+    player = Player();
     add(player);
   }
 
   @override
   void onPanUpdate(DragUpdateInfo info) {
     player.move(info.delta.global);
+  }
+
+  @override
+  void onPanStart(DragStartInfo info) {
+    player.startShooting();
+  }
+
+  @override
+  void onPanEnd(DragEndInfo info) {
+    player.stopShooting();
   }
 }
 
